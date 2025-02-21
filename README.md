f0305818bf90a13a6d44197be6c69a941f6614e6
lucasdbrown <42070926+lucasdbrown@users.noreply.github.com>
2025-02-21 14:32:31 -0500

    hmi app and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# OT Network Template
+# OT Virtualized Network Template
 Simulated OT Network Template for OT Security Competitions
 
 
 ## Setting up your OT Network
-Go to the `docs` folder and go through all the individual markdown files.
+- Go to the `docs` folder and go through all the individual markdown files.
+
+
+
+## Guide for how to make your own OT Network using this template
+- Make your own simulated python components and add there configurations to the `docker-compose.yml` in the `simulated_components` folder
+
+
+
+## Credit
+- Couldn't have made this thesis without the creation of Virtue Pot by Nikhil Karakuchi (https://github.com/0xnkc/virtuepot/), you can find his Master's Thesis paper on VirtuePot here: https://thesis.unipd.it/handle/20.500.12608/71043
---
+++ industrial_component/hmi/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, current_app
+from pymodbus.client.sync import ModbusTcpClient
+from datetime import datetime
+from flask import jsonify
+import secrets
+
+
+app = Flask(__name__)
+
+
+secret_key = secrets.token_hex(16)
+# example output, secret_key = 000d88cd9d90036ebdd237eb6b0db000
+app.config['SECRET_KEY'] = secret_key
+
+# datetime object containing current date and time
+def current_time():
+    now = datetime.now().isoformat()
+    return now
+
+@app.route('/api',methods=['POST','GET'])
+def api():
+    host = '192.168.0.26'
+    port = 502
+    client = ModbusTcpClient(host, port)
+    client.connect()
+    result = client.read_holding_registers(101,10,unit=0)
+    data = {
+            "datetime": current_time(),
+            "data": result.registers 
+        }
+    return jsonify(data)
+
+@app.route('/')
+def index():
+    return (render_template('index.html'))
+
+if __name__ == '__main__':
+    app.run()
