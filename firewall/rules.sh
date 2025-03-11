00d6c2f404e79bfd8bac22b36ed936c08c400997
lucasdbrown <42070926+lucasdbrown@users.noreply.github.com>
2025-03-11 17:27:03 -0400

    comments to the firewall rules bash script

--- firewall/rules.sh
+++ firewall/rules.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Flushs existing rules
+# Flushs (all three included in flushing) existing rules
 iptables -F
 iptables -X
 iptables -Z
 
 # Default policies (Drops all traffic by default)
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT  # Allows outbound traffic
+iptables -P INPUT DROP # blocks all incoming traffic
+iptables -P FORWARD DROP # blocks all forwarded traffic by routers
+iptables -P OUTPUT ACCEPT  # Allows firewall to send outbound traffic
 
-# Allow Loopback
+# Allow Loopback (for local communication)
 iptables -A INPUT -i lo -j ACCEPT
 
-# Enterprise -> IDMZ (Allows only Web Proxy Access)
+# Enterprise -> IDMZ (Allows only Web Proxy Access (on ports 80 & 443))
 iptables -A FORWARD -s 192.168.10.0/24 -d 192.168.20.2 -p tcp --dport 80 -j ACCEPT
 iptables -A FORWARD -s 192.168.10.0/24 -d 192.168.20.2 -p tcp --dport 443 -j ACCEPT
 
