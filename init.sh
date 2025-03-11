7d5296f40c66331554ce604347a2010cc1803e13
Lukas May <lukas7may@gmail.com>
2025-03-11 19:07:25 -0400

    Everything runs together

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 
 docker compose -f network/docker-compose.yml up -d
-# docker compose -f enterprise/docker-compose.yml up -d
-# docker compose -f idmz/docker-compose.yml up -d
-# docker compose -f firewall/docker-compose.yml up -d
+docker compose -f enterprise/docker-compose.yml up -d
+docker compose -f idmz/docker-compose.yml up -d
+docker compose -f firewall/docker-compose.yml up -d
 docker compose -f industrial/docker-compose.yml up -d
 
--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 docker compose -f network/docker-compose.yml down 
 docker compose -f enterprise/docker-compose.yml down
 docker compose -f idmz/docker-compose.yml down 
+docker compose -f firewall/docker-compose.yml down
 docker compose -f industrial/docker-compose.yml down
 
 docker network prune -f
