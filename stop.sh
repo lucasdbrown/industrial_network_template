c4b192b27c1147e9197003b6e9ff639c0d3080fa
Lukas May <lukas7may@gmail.com>
2025-03-11 15:44:04 -0400

    starting stopping scripts

---
+++ stop.sh
@@ -0,0 +1,5 @@
+docker compose -f network/docker-compose.yml down 
+docker compose -f enterprise/docker-compose.yml down
+docker compose -f idmz/docker-compose.yml down 
+docker compose -f industrial/docker-compose.yml down
+
