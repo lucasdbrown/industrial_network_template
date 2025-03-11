dd26c07ba1a923cda5a81e687e3cca0e363ba0fd
Lukas May <lukas7may@gmail.com>
2025-03-11 16:20:55 -0400

    feat: The networks work now

--- stop.sh
+++ stop.sh
@@ -3,3 +3,5 @@ docker compose -f enterprise/docker-compose.yml down
 docker compose -f idmz/docker-compose.yml down 
 docker compose -f industrial/docker-compose.yml down
 
+docker network prune -f
+# docker system prune
