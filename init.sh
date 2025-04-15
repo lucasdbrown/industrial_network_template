583f195eeeb2b416523d164d1be0acaf98da4db4
Lukas May <lukas7may@gmail.com>
2025-04-14 22:39:24 -0400

    fix(file name): DockerFile renamed to Dockerfile

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
-
 docker compose -f network/docker-compose.yml up -d
-# docker compose -f enterprise/docker-compose.yml up -d
-# docker compose -f idmz/docker-compose.yml up -d
-# docker compose -f firewall/docker-compose.yml up -d
-# docker compose -f industrial/docker-compose.yml up -d
+docker compose -f enterprise/docker-compose.yml up -d
+docker compose -f idmz/docker-compose.yml up -d
 docker compose -f industrial/docker-compose.yml up -d
 docker compose -f logging/docker-compose.yml up -d
